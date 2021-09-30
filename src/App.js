857123566b8e8f72a1de3a0db33ce1ed20ef1ef5
Manish Jha <manishjhazyx@gmail.com>
2021-09-30 21:14:24 +0530

    added movie descriptions

--- src/App.js
+++ src/App.js
@@ -5,43 +5,60 @@ const moviesList = {
   Action: [
     {
       Name: "Dhoom 2",
-      Rating: "4/5"
+      Rating: "4/5",
+      Description:
+        "A cat and mouse tale between Superchor Hrithik and supercop Abhishek."
     },
     {
       Name: "The Raid",
-      Rating: "5/5"
+      Rating: "5/5",
+      Description:
+        "A korean action movie where the hero fights across the floor of a building to reach the villain at the top."
     },
     {
       Name: "Mission Impossible",
-      Rating: "5/5"
+      Rating: "5/5",
+      Description:
+        "Tom Cruise plays Ethan Hunt, an agent of the Impossible Mission force, on his most dangerous mission yet."
     }
   ],
   Thriller: [
     {
       Name: "Fan",
-      Rating: "4/5"
+      Rating: "4/5",
+      Description:
+        "Shahrukh Khan plays a version of his superstar self and his biggest fan!"
     },
     {
       Name: "Race",
-      Rating: "3/5"
+      Rating: "3/5",
+      Description: "Twist after twist after twist!!!"
     },
     {
       Name: "The Mummy",
-      Rating: "4/5"
+      Rating: "4/5",
+      Description:
+        "An ancient mummy has awakened in Egypt and the world is going to feel its wrath!"
     }
   ],
   Comedy: [
     {
       Name: "Phir Hera Pheri",
-      Rating: "5/5"
+      Rating: "5/5",
+      Description:
+        "A sequel to the hilarious 'Hera Pheri' taking off after the events of the first movie. "
     },
     {
       Name: "Dhamaal",
-      Rating: "4/5"
+      Rating: "4/5",
+      Description:
+        "Four idiot friends in possesion of a treasure map undertake a hilarious journey to find said treasure."
     },
     {
       Name: "Baby's Day Out",
-      Rating: "4/5"
+      Rating: "4/5",
+      Description:
+        "A baby is kidnapped by three criminals. It is bad news, for the criminals..."
     }
   ]
 };
@@ -104,6 +121,9 @@ export default function App() {
           >
             <div style={{ fontSize: "larger", marginBottom: "0.5rem" }}>
               {movie.Name}
+            </div>
+            <div style={{ fontSize: "smaller", marginBottom: "0.5rem" }}>
+              {movie.Description}
             </div>
             <div style={{ fontSize: "smaller" }}>{movie.Rating}</div>
           </li>
